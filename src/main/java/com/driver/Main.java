b649c49353103349d837558faddc78876986949a
Navify97 <navneetji97@gmail.com>
2023-02-08 20:22:49 +0530

    first changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
 
 public class Main {
+    class Product{
+        public int product(int x, int y) {
+
+        }
+        public int product(int x, int y, int z) {
+
+        }
+        public double product(double x, double y) {}
+    }
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product();
+        p.product();
+        p.product();
+    }
 
 }
